3969b32f9c1a9101ceb571bc4bb0a54dc156a870
harshilmalhotra <malhotraharshil2@gmail.com>
2024-03-24 16:49:09 +0530

    markdown-feature-all-working

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,16 +7,7 @@ export default {
   ],
 
   theme: {
-    extend: {
-      // fontSize: {
-      //   'h1': '48px',
-      //   'h2': '36px',
-      //   'h3': '30px',
-      //   'h4': '24px',
-      //   'h5': '20px',
-      //   'h6': '16px',
-      // },
-    },
+    extend: {},
   },
 
   variants: {},
